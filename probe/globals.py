fbad46655791ea78cf8bbe98dbb484c31549af86
Lily Espirito Santo <lily.esanto@gmail.com>
2020-10-21 17:09:25 +0200

    Fix typo for dataset_id
    Reset version

--- probe/globals.py
+++ probe/globals.py
@@ -3,5 +3,5 @@ import uuid
 
 event_interval = int(os.getenv("EVENT_INTERVAL_SECONDS"))
 max_consecutive_errors = int(os.getenv("MAX_CONSECUTIVE_ERRORS"))
-dataset_id = os.getenv("DATASET_IT")
+dataset_id = os.getenv("DATASET_ID")
 app_id = str(uuid.uuid4())[:8]

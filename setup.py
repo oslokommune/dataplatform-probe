f219e5544e9a1edfe35fdc40971e3dc20de8cb46
Fredrik Vraalsen <fredrik@vraalsen.no>
2021-08-05 10:18:58 +0200

    DP-2054 Add setup.py
    To make e.g. 'make upgrade-deps' work

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="dataplatform-probe",
+    version="0.1.0",
+    author="Origo Dataplattform",
+    author_email="dataplattform@oslo.kommune.no",
+    description="Monitoring service for dataplatform services and events",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oslokommune/dataplatform-probe",
+    packages=find_packages(),
+    install_requires=[
+        "aws-xray-sdk",
+        "okdata-aws",
+        "prometheus-client",
+        "requests",
+        "tabulate",
+        "websockets",
+    ],
+)
